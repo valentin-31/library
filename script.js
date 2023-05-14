b18d1865c5195e3a23003f064bc68af5f33fb8b6
valentin <valen.manestar@gmail.com>
2023-05-14 01:35:29 -0300

    Añadido un formulario para el registro de libros

--- script.js
+++ script.js
@@ -9,16 +9,25 @@ function Book (title, author, pages, read) {
   this.read = read
 
   this.info = function () {
-    return title + 'by ' + author + pages + read
+    return title + ' by ' + author + ' has ' + pages + ' pages. ' + 'It has been readed? ' + read
   }
 }
 
 const B01 = new Book('VillaLaConchaDeTuMadre', 'Yo', '69', 'yes')
 
+// This function opens a form to add a book
+function openForm () {
+  document.getElementById('bookForm').style.display = 'block'
+}
+
+function closeForm () {
+  document.getElementById('bookForm').style.display = 'none'
+}
+
 // Esta funcion toma input del user
 function getBookData () {
   const title = window.prompt('What is the title of the book?')
-  const author = window.prompt('What is the title of the book?')
+  const author = window.prompt('Who is the author of the book?')
   const pages = window.prompt('Whats is te number of pages?')
   const read = window.prompt('Have you read the book?')
 
@@ -32,3 +41,11 @@ function addBookToLibrary (book) {
 }
 
 addBookToLibrary(B01)
+
+// Function that loop through the array and displays each book
+
+function displayBook () {
+  myLibrary.forEach(book => {
+    return alert(book.info())
+  })
+}
